9a684c2c40f0d2816e52cc49f421fdbd91c10056
pablo maya <pmaya@enacment.com>
2019-12-24 00:49:48 -0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+Use App\Article;
+
+Route::post('register', 'Auth\RegisterController@register'); 
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
